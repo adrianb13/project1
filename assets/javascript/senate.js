3f4f31bbe2b4b07d4578f8daddae64e84c9b3c8b
Adrian Brinoes <Coding4Adrian@yahoo.com>
2019-01-25 21:00:08 -0800

    senate.js

--- assets/javascript/senate.js
+++ assets/javascript/senate.js
@@ -3,8 +3,13 @@
 
 $(document).ready(function(){
 
-    function congressInfo () {
-        var state = "ca"
+    $("#find").on("click", function congressInfo () {
+        $("#infoTable > tbody").empty();
+        $("#result1").empty();
+        $("#result2").empty();
+        $("#name1").empty();
+        $("#name2").empty();
+        var state = $("#userState").val().trim();
         var searchURL = "https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyConiWRnN46UCfMvBRUESD_MvTF2ucsnNE&address=" + state;
         
         $.ajax({
@@ -18,23 +23,23 @@ $(document).ready(function(){
             var party = response.officials[j].party;
             var address = response.officials[j].address[0].line1 + ", " + response.officials[j].address[0].city + ", " + response.officials[j].address[0].state + " " + response.officials[j].address[0].zip;
             var phone = response.officials[j].phones[0];
-            var url = response.officials[j].urls[0];
+                state = response.normalizedInput.state;
+//            var url = response.officials[j].urls[0];
 
             var newRow = $("<tr>").append(
                 $("<td>").text(name),
                 $("<td>").text(party),
                 $("<td>").text(address),
                 $("<td>").text(phone),
-                $("<td>").text(url)
+//                $("<td>").text(url)
             );
             
             $("#infoTable > tbody").append(newRow)
             }
 
         }).then(function(response) {
-            var chamber = "senate";
-            var state = "ca"
-            var queryURL = "https://api.propublica.org/congress/v1/members/senate/CA/current.json";
+            var chamber = "senate"
+            var queryURL = "https://api.propublica.org/congress/v1/members/senate/" + state + "/current.json";
         
             $.ajax({
                 url: queryURL,
@@ -45,25 +50,40 @@ $(document).ready(function(){
                 }
             }).then(function(response) {
                 console.log(response);
-                $("#results").empty();
                 console.log(response.copyright)
-//                $("#copyright").text(response.copyright)
-/*                for (i = 0; i < 2; i++) {
+                $("#copyright").text(response.copyright)
+                for (i = 0; i < 2; i++) {
                     var name = response.results[i].name;
-                    var polParty = response.results[i].party;
-                    var id = response.results[i].id; 
-        
-                    var newRow = $("<tr>").append(
-                        $("<td>").text(name),
-                        $("<td>").text(polParty),
-                        $("<td>").text(id)
-                    ); 
-*/
-//              $("#infoTable > tbody").append(newRow)
-//              }
-                })
-            })    
-        
-    }
-    congressInfo();
+                    var party = response.results[i].party;
+                    var bioGuideID = response.results[i].id; 
+                    var photoURL = "https://theunitedstates.io/images/congress/225x275/" +bioGuideID + ".jpg";
+
+// Photo Styline & Upload
+                    var photo = $("<img>");
+                        photo.attr("src", photoURL);
+                        photo.attr("class", "imageSpacing");
+
+// Red/Blue Background
+                    if (party === "D") {
+                        photo.attr("class", "blue")
+                    } else if (party === "R") {
+                        photo.attr("class", "red")
+                    }; console.log(party)
+
+// Photo box determination
+                    if (i === 0) {
+                        $("#result1").append(photo);
+                        $("#name1").text(name);
+                    } else if (i === 1) {
+                        $("#result2").append(photo);
+                        $("#name2").text(name);
+                    };
+                };
+            });
+        });
+
+        $("#userState").val("");
+    });
+    
+//    congressInfo();
 });
